a44523de21216dff3010e3fb48d8040587fd9f9e
Redete <bezabehredete1@gmail.com>
2024-08-27 17:53:14 +0000

    adding functions v2

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-#my_ip=
-#my_username=whoami
+ask_choice() {
+    echo "What would you like to check?
+    (1) My Public and Private Addresses
+    (2) My Username
+    (3) Free CPU Amount
+    (4) Free Memory Amount
+    (5) Top 5 Memory Processes
+    (6) Top 5 CPU Processes
+    (7) Network Connectivity
+    (8) Exit"
+
+    read choice
+}
+
+print_myusername() {
+    echo "123"
+}
+
+print_username() {
+    echo "$(whoami)"
+}
+
 #free_cpu=
 #free_memory=
 #top_memory=
@@ -28,7 +48,7 @@ elif [ "$choice" == "1" ]; then
     echo "123"
 
 elif [ "$choice" == "2" ]; then
-    echo "whoami"
+    print_username
 
 elif [ "$choice" == "3" ]; then
     echo "CPU"
